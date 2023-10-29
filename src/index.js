37161d68026f460a17cd854e13d7f45061114a8b
saul sanchez <saul_sanchezz@live.com>
2023-10-29 13:04:32 -0700

    require

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import conectarDB from "./config/db.js"
-import usuarioRoutes from "./routes/usuarioRoutes.js"
-import manifiestoRoutes from "./routes/manifiestoRoutes.js"
-import tamanoRoutes from "./routes/tamanoRoutes.js"
-import cultivoRoutes from "./routes/cultivoRoutes.js"
-import marcaRoutes from "./routes/marcaRoutes.js"
-import envaseRoutes from "./routes/envaseRoutes.js"
-import expoManifiestoRotes from "./routes/expoManifiestoRoutes.js"
-import nombreEmpresaRoutes from "./routes/nombreEmpresaRoutes.js"
-import lineaTransporteRoutes from "./routes/lineaTransporteRoutes.js"
-import choferRoutes from "./routes/choferRoutes.js"
-import trailerRoutes from "./routes/trailerRoutes.js"
-import cajaRoutes from "./routes/cajaRoutes.js"
-import pisoAnteriorRoutes from "./routes/pisoAnteriorRoutes.js"
-import pisoActualRoutes from "./routes/pisoActualRoutes.js"
-import palletRoutes from "./routes/palletRoutes.js"
-import productoInventarioRoutes from "./routes/productoInventarioRoutes.js"
-import reporteEmpaqueRoutes from "./routes/reporteEmpaqueRoutes.js"
+const express = require("express");
+// import express from "express"
+// import dotenv from "dotenv"
+// import cors from "cors"
+// import conectarDB from "./config/db.js"
+// import usuarioRoutes from "./routes/usuarioRoutes.js"
+// import manifiestoRoutes from "./routes/manifiestoRoutes.js"
+// import tamanoRoutes from "./routes/tamanoRoutes.js"
+// import cultivoRoutes from "./routes/cultivoRoutes.js"
+// import marcaRoutes from "./routes/marcaRoutes.js"
+// import envaseRoutes from "./routes/envaseRoutes.js"
+// import expoManifiestoRotes from "./routes/expoManifiestoRoutes.js"
+// import nombreEmpresaRoutes from "./routes/nombreEmpresaRoutes.js"
+// import lineaTransporteRoutes from "./routes/lineaTransporteRoutes.js"
+// import choferRoutes from "./routes/choferRoutes.js"
+// import trailerRoutes from "./routes/trailerRoutes.js"
+// import cajaRoutes from "./routes/cajaRoutes.js"
+// import pisoAnteriorRoutes from "./routes/pisoAnteriorRoutes.js"
+// import pisoActualRoutes from "./routes/pisoActualRoutes.js"
+// import palletRoutes from "./routes/palletRoutes.js"
+// import productoInventarioRoutes from "./routes/productoInventarioRoutes.js"
+// import reporteEmpaqueRoutes from "./routes/reporteEmpaqueRoutes.js"
 
 const app = express()
 app.use(express.json())
@@ -46,23 +47,23 @@ app.use(cors(corsOptions))
 app.use("/", (req, res) => {
   res.json({"msg": "servidor corriendo..."})
 })
-app.use("/api/usuarios", usuarioRoutes)
-app.use("/api/nacional-manifiestos", manifiestoRoutes)
-app.use("/api/tamanos", tamanoRoutes)
-app.use("/api/cultivos", cultivoRoutes)
-app.use("/api/marcas", marcaRoutes)
-app.use("/api/envases", envaseRoutes)
-app.use("/api/expo-manifiestos", expoManifiestoRotes)
-app.use("/api/empresas", nombreEmpresaRoutes)
-app.use("/api/linea-transportes", lineaTransporteRoutes)
-app.use("/api/choferes", choferRoutes)
-app.use("/api/trailers", trailerRoutes)
-app.use("/api/cajas", cajaRoutes)
-app.use("/api/piso-anterior", pisoAnteriorRoutes)
-app.use("/api/piso-actual", pisoActualRoutes)
-app.use("/api/pallets", palletRoutes)
-app.use("/api/inventario-productos", productoInventarioRoutes)
-app.use("/api/reportes-empaque", reporteEmpaqueRoutes)
+// app.use("/api/usuarios", usuarioRoutes)
+// app.use("/api/nacional-manifiestos", manifiestoRoutes)
+// app.use("/api/tamanos", tamanoRoutes)
+// app.use("/api/cultivos", cultivoRoutes)
+// app.use("/api/marcas", marcaRoutes)
+// app.use("/api/envases", envaseRoutes)
+// app.use("/api/expo-manifiestos", expoManifiestoRotes)
+// app.use("/api/empresas", nombreEmpresaRoutes)
+// app.use("/api/linea-transportes", lineaTransporteRoutes)
+// app.use("/api/choferes", choferRoutes)
+// app.use("/api/trailers", trailerRoutes)
+// app.use("/api/cajas", cajaRoutes)
+// app.use("/api/piso-anterior", pisoAnteriorRoutes)
+// app.use("/api/piso-actual", pisoActualRoutes)
+// app.use("/api/pallets", palletRoutes)
+// app.use("/api/inventario-productos", productoInventarioRoutes)
+// app.use("/api/reportes-empaque", reporteEmpaqueRoutes)
 
 
 const PORT = 4000
